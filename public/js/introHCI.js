443586401c45548551451c015f22b38549648f82
Jessica Ly <lyjessica28@gmail.com>
2016-02-17 10:28:11 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeCtr").click(function(e) {
+		e.preventDefault();
+		ga("send", "event", 'like', 'click');
+	});
 }
